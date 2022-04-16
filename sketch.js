be0f993ffd728827802908fda2c4de5df823ebb2
Vaibhav0603 <99812391+Vaibhav0603@users.noreply.github.com>
2022-04-16 18:07:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,193 @@
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var boy, boy_running, boy_collided;
+var ground, invisibleGround, groundImage;
+
+var cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+
+var score=0;
+
+var gameOver, restart;
+
+
+
+function preload(){
+  boy_running =   loadAnimation("boy1.png","boy2.png","boy3.png","boy4.png","boy5.png","boy6.png");
+  boy_collided = loadAnimation("boy_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+
+  cloudImage = loadImage("cloud.png");
+  
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
+  obstacle5 = loadImage("obstacle5.png");
+  obstacle6 = loadImage("obstacle6.png");
+  
+  gameOverImg = loadImage("gameOver.png");
+  restartImg = loadImage("restart.png");
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  
+  boy = createSprite(50,180,20,50);
+  
+  boy.addAnimation("running", boy_running);
+  boy.addAnimation("collided",boy_collided);
+  boy.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -(6 + 7*score/200);
+  
+  gameOver = createSprite(300,100);
+  gameOver.addImage(gameOverImg);
+  
+  restart = createSprite(300,140);
+  restart.addImage(restartImg);
+  
+  gameOver.scale = 0.5;
+  restart.scale = 0.5;
+
+  gameOver.visible = false;
+  restart.visible = false;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  cloudsGroup = new Group();
+  obstaclesGroup = new Group();
+  
+  score = 0;
+}
+
+function draw() {
+
+  background(255);
+  text("Score: "+ score, 500,50);
+  
+  if (gameState===PLAY){
+    score = score + Math.round(getFrameRate()/60);
+    ground.velocityX = -(6 + 3*score/100);
+  
+    if((touches.length>0||keyDown("space")) && boy.y >= 159) {
+      boy.velocityY = -14;
+      touches = []
+    }
+  
+    boy.velocityY = boy.velocityY + 0.8
+  
+    if (ground.x < 0){
+      ground.x = ground.width/2;
+    }
+  
+    boy.collide(invisibleGround);
+    spawnClouds();
+    spawnObstacles();
+  
+    if(obstaclesGroup.isTouching(boy)){
+        gameState = END;
+    }
+  }
+  else if (gameState === END) {
+    gameOver.visible = true;
+    restart.visible = true;
+    
+
+    ground.velocityX = 0;
+    boy.velocityY = 0;
+    obstaclesGroup.setVelocityXEach(0);
+    cloudsGroup.setVelocityXEach(0);
+    
+
+    boy.changeAnimation("collided",boy_collided);
+    
+
+    obstaclesGroup.setLifetimeEach(-1);
+    cloudsGroup.setLifetimeEach(-1);
+    
+    if(mousePressedOver(restart)) {
+      reset();
+    } 
+  }
+  
+  
+  drawSprites();
+}
+
+function spawnClouds() {
+
+  if (frameCount % 60 === 0) {
+    var cloud = createSprite(600,120,40,10);
+    cloud.y = Math.round(random(80,120));
+    cloud.addImage(cloudImage);
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
+    
+
+    cloud.lifetime = 200;
+    cloud.depth = boy.depth;
+    boy.depth = boy.depth + 1;
+    
+
+    cloudsGroup.add(cloud);
+  }
+  
+}
+
+function spawnObstacles() {
+  if(frameCount % 60 === 0) {
+    var obstacle = createSprite(600,165,10,40);
+
+    obstacle.velocityX = -(6 + 3*score/100);
+
+    var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1: obstacle.addImage(obstacle1);
+              break;
+      case 2: obstacle.addImage(obstacle2);
+              break;
+      case 3: obstacle.addImage(obstacle3);
+              break;
+      case 4: obstacle.addImage(obstacle4);
+              break;
+      case 5: obstacle.addImage(obstacle5);
+              break;
+      case 6: obstacle.addImage(obstacle6);
+              break;
+      default: break;
+    }
+    
+          
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 300;
+  
+    obstaclesGroup.add(obstacle);
+  }
+}
+
+function reset(){
+  gameState = PLAY;
+  gameOver.visible = false;
+  restart.visible = false;
+  
+  obstaclesGroup.destroyEach();
+  cloudsGroup.destroyEach();
+  
+  boy.changeAnimation("running",boy_running);
+  
+ 
+  
+  score = 0;
+  
+}
+   
+  
+
